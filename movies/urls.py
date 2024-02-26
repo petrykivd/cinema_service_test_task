3ce810138aa79d7bce54b2052a1006ac29034124
petrykivd <petrykiv.dmytro19@gmail.com>
2024-02-26 19:49:51 +0200

    Implemented movie crud.

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
 
 from .views import (
-    index, MovieListView,
+    index,
+    MovieListView,
+    MovieCreateView,
+    MovieUpdateView,
+    MovieDeleteView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("movies/", MovieListView.as_view(), name="movie-list"),
+    path(
+        "movies/create/",
+        MovieCreateView.as_view(),
+        name="movie-create",
+    ),
+    path(
+        "movies/<int:pk>/update/",
+        MovieUpdateView.as_view(),
+        name="movie-update",
+    ),
+    path(
+        "movies/<int:pk>/delete/",
+        MovieDeleteView.as_view(),
+        name="movie-delete",
+    ),
 ]
 
 app_name = "movies"
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from movies.forms import MovieNameSearchForm
 from movies.models import Movie, Actor, Genre
 
 
@@ -16,4 +18,42 @@ def index(request):
 
 class MovieListView(generic.ListView):
     model = Movie
-    paginate_by = 25
+    context_object_name = "movie_list"
+    template_name = "movies/movie_list.html"
+    paginate_by = 24
+    queryset = Movie.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(MovieListView, self).get_context_data(**kwargs)
+        title = self.request.GET.get("title", "")
+        context["search_form"] = MovieNameSearchForm(
+            initial={
+                "title": title
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        form = MovieNameSearchForm(self.request.GET)
+        if form.is_valid():
+            return self.queryset.filter(
+                title__icontains=form.cleaned_data["title"]
+            )
+        return self.queryset
+
+
+class MovieCreateView(generic.CreateView):
+    model = Movie
+    fields = "__all__"
+    success_url = reverse_lazy("movies:movie-list")
+
+
+class MovieUpdateView(generic.UpdateView):
+    model = Movie
+    fields = "__all__"
+    success_url = reverse_lazy("movies:movie-list")
+
+
+class MovieDeleteView(generic.DeleteView):
+    model = Movie
+    success_url = reverse_lazy("movies:movie-list")

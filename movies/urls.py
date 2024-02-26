70c6fb7d51351e96883d93b97be06ee25e25e9a7
petrykivd <petrykiv.dmytro19@gmail.com>
2024-02-26 21:25:36 +0200

    Added link to detail view for every movie in list

--- movies/urls.py
+++ movies/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("movies/", MovieListView.as_view(), name="movie-list"),
-    path("movies/<int:pk>/", MovieDetailView.as_view(), name="moview-detail"),
+    path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
     path(
         "movies/create/",
         MovieCreateView.as_view(),
